748fb423f35966e53020272b2437d61399b518eb
tien <tienqhwork@gmail.com>
2023-08-30 18:25:46 +0700

    tiến đẹp trai

--- MyProject/App.js
+++ MyProject/App.js
@@ -16,16 +16,12 @@ const Stack = createStackNavigator();
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 
+
 const Tab = createBottomTabNavigator();
 
 const HomeTabs = ({navigation}) => {
-
- 
-
   return (
     <View style={{flex: 1}}>
- 
-
       <Tab.Navigator
         initialRouteName="Đoạn Chat"
         screenOptions={{
@@ -40,7 +36,10 @@ const HomeTabs = ({navigation}) => {
                 justifyContent: 'center',
                 alignItems: 'center',
               }}>
-              <TouchableOpacity onPress={()=>{navigation.openDrawer()}}>
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.openDrawer();
+                }}>
                 <Image
                   source={require('./src/Images/menu.png')}
                   style={{width: 20, height: 20, alignItems: 'center'}}
@@ -61,7 +60,51 @@ const HomeTabs = ({navigation}) => {
                 resizeMode="stretch"
               />
             ),
-            headerRight: () => <View></View>,
+            headerRight: () => (
+              <View style={{flexDirection: 'row'}}>
+                <View
+                  style={{
+                    backgroundColor: '#C0C0C0',
+                    marginLeft: 6,
+                    width: 40,
+                    height: 40,
+                    borderRadius: 50,
+                    marginRight: 5,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}>
+                  <TouchableOpacity>
+                    <Image
+                      source={require('./src/Images/camera.png')}
+                      style={{width: 20, height: 20, alignItems: 'center'}}
+                      resizeMode="stretch"
+                    />
+                  </TouchableOpacity>
+                </View>
+                <View
+                  style={{
+                    backgroundColor: '#C0C0C0',
+                    marginLeft: 6,
+                    width: 40,
+                    height: 40,
+                    marginRight: 5,
+                    borderRadius: 50,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}>
+                  <TouchableOpacity
+                    onPress={() => {
+                      navigation.openDrawer();
+                    }}>
+                    <Image
+                      source={require('./src/Images/pencil.png')}
+                      style={{width: 20, height: 20, alignItems: 'center'}}
+                      resizeMode="stretch"
+                    />
+                  </TouchableOpacity>
+                </View>
+              </View>
+            ),
           }}
         />
         <Tab.Screen
@@ -75,6 +118,48 @@ const HomeTabs = ({navigation}) => {
                 resizeMode="stretch"
               />
             ),
+            headerRight:()=>(
+              <View style = {{flexDirection:'row'}}>
+               <View
+              style={{
+                backgroundColor: '#C0C0C0',
+                marginLeft: 6,
+                width: 40,
+                height: 40,
+                borderRadius: 50,
+                marginRight:5,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <TouchableOpacity >
+                <Image
+                  source={require('./src/Images/phone.png')}
+                  style={{width: 20, height: 20, alignItems: 'center'}}
+                  resizeMode="stretch"
+                />
+              </TouchableOpacity>
+            </View>
+            <View
+              style={{
+                backgroundColor: '#C0C0C0',
+                marginLeft: 6,
+                width: 40,
+                height: 40,
+                marginRight:5,
+                borderRadius: 50,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <TouchableOpacity >
+                <Image
+                  source={require('./src/Images/video-calling-app.png')}
+                  style={{width: 20, height: 20, alignItems: 'center'}}
+                  resizeMode="stretch"
+                />
+              </TouchableOpacity>
+            </View>
+              </View>
+          ),
           }}
         />
         <Tab.Screen
@@ -87,7 +172,28 @@ const HomeTabs = ({navigation}) => {
                 style={{width: 20, height: 20}}
                 resizeMode="stretch"
               />
+            ),headerRight:()=>(
+              <View
+              style={{
+                backgroundColor: '#C0C0C0',
+                marginLeft: 6,
+                width: 40,
+                height: 40,
+                marginRight:5,
+                borderRadius: 50,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <TouchableOpacity >
+                <Image
+                  source={require('./src/Images/contacts.png')}
+                  style={{width: 20, height: 20, alignItems: 'center'}}
+                  resizeMode="stretch"
+                />
+              </TouchableOpacity>
+            </View>
             ),
+            
           }}
         />
         <Tab.Screen
@@ -101,11 +207,11 @@ const HomeTabs = ({navigation}) => {
                 resizeMode="stretch"
               />
             ),
+
+            
           }}
         />
       </Tab.Navigator>
-
-
     </View>
   );
 };
@@ -115,9 +221,14 @@ const HomeMesenger = () => {
     <NavigationContainer>
       <Drawer.Navigator
         screenOptions={{headerShown: false}}
-        initialRouteName="HoameTab">
-        <Stack.Screen name="HoameTab" component={HomeTabs} />
-        <Drawer.Screen name="Chat" component={ChatComponent} options={{}} />
+        initialRouteName="Đoạn Chat">
+        <Stack.Screen name="Đoạn Chat" component={HomeTabs} options={{
+        
+          
+        }}/>
+        <Drawer.Screen name="Chat" component={ChatComponent} options={{
+         
+        }} />
         <Drawer.Screen name="Call" component={CallComponent} />
         <Drawer.Screen name="PhoneBook" component={PhoneBookComponent} />
         <Drawer.Screen name="News" component={NewsComponent} />
--- MyProject/src/Components/ChatComponent.js
+++ MyProject/src/Components/ChatComponent.js
@@ -1,15 +1,167 @@
-import { StyleSheet, Text, View , SafeAreaView , Image} from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TextInput,
+  Dimensions,
+  FlatList,
+  ScrollView,
+  ImageBackground,
+} from 'react-native';
+import React, {useState} from 'react';
+import {relative} from 'path';
+
+const {width, height} = Dimensions.get('window');
+const data = [
+  {
+    id: '1',
+    name: 'Hoàng Anh  Nguyệt',
+    image:
+      'https://antimatter.vn/wp-content/uploads/2022/10/hinh-anh-3d-800x500.jpg',
+      status:1 , 
+      message:[]
+  },
+  {
+    id: '2',
+    name: 'Hà Trọng Huy',
+    image:
+      'https://antimatter.vn/wp-content/uploads/2022/10/hinh-anh-3d-800x500.jpg',
+      status:0
+       , message:[]
+  },
+  {
+    id: '3',
+    name: 'Nguyễn Đức Tùng',
+    image:
+      'https://antimatter.vn/wp-content/uploads/2022/10/hinh-anh-3d-800x500.jpg',
+      status:1
+      , message:[]
+  },
+  {
+    id: '4',
+    name: 'Hồng Hạnh',
+    image:
+      'https://antimatter.vn/wp-content/uploads/2022/10/hinh-anh-3d-800x500.jpg',
+      message:[]
+  },
+  {
+    id: '5',
+    name: 'Đào Toàn',
+    image:
+      'https://antimatter.vn/wp-content/uploads/2022/10/hinh-anh-3d-800x500.jpg',
+      status:1
+      , message:[]
+  },
+  {
+    id: '6',
+    name: 'Đỗ Duy Mạnh',
+    image:
+      'https://antimatter.vn/wp-content/uploads/2022/10/hinh-anh-3d-800x500.jpg',
+      status:1
+      , message:[]
+  },
+  {
+    id: '7',
+    name: 'Quán Văn Luân',
+    image:
+      'https://antimatter.vn/wp-content/uploads/2022/10/hinh-anh-3d-800x500.jpg',
+      status:0
+      , message:[]
+  },
+  {
+    id: '8',
+    name: 'Vũ Việt Hải',
+    image:
+      'https://antimatter.vn/wp-content/uploads/2022/10/hinh-anh-3d-800x500.jpg',
+      status:1
+      , message:[]
+  },
+  // Thêm các mục khác vào đây
+];
+const renderItem = ({item}) => (
+  <View style={{flexDirection: 'row'}}>
+    <Text>{item.name}</Text>
+  </View>
+);
+
 
 const ChatComponent = () => {
+
+  
+  const [search, setsearch] = useState('');
   return (
-  <SafeAreaView >
-    <Text>1234</Text>
-    <Image source={require('../Images/chat.png')} style={{ width: 20, height: 20 }} resizeMode="contain" />
-  </SafeAreaView>
-  )
-}
+    <SafeAreaView>
+      <View style={{flexDirection: 'row', alignItems: 'center'}}>
+        <TextInput
+          value={search}
+          placeholder="Tìm Kiếm"
+          onChangeText={text => setsearch(text)}
+          style={{
+            width: width * 0.9,
+            height: 50,
+            backgroundColor: '#C0C0C0',
+            margin: 10,
+            borderRadius: 50,
+            paddingLeft: 35,
+            fontSize: 15,
+          }}></TextInput>
+        <Image
+          source={require('./../Images/search.png')}
+          style={{width: 20, height: 20, position: 'absolute', left: 20}}
+        />
+      </View>
 
-export default ChatComponent
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        <View style={{flexDirection: 'row'}}>
+          {data.map(item => (
+            <View
+              key={item.id}
+              style={{
+                width: 80,
+                height: 110,
+                marginRight: 10,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <Image
+                source={{uri: `${item.image}`}}
+                style={{
+                  width: 60,
+                  height: 60,
+                  borderRadius: 50,
+                }}
+              />
+          {
+            item.status==1?    <View
+            style={{
+              width: 15,
+              height: 15,
+              borderRadius: 50,
+              backgroundColor: 'green',
+              marginTop: -15,
+              marginRight: -38,
+            }}></View>:null
+          }
+              <View
+                style={{
+                  flex: 1,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                <Text numberOfLines={2} style={{textAlign: 'center'}}>
+                  {item.name}
+                </Text>
+              </View>
+            </View>
+          ))}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default ChatComponent;
+
+const styles = StyleSheet.create({});
